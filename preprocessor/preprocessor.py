0e45d8056ad199e3521c3b0c3ba588d0dff41f7f
Matheus Centa <matheuscenta@gmail.com>
2019-12-05 20:17:15 +0100

    Eliminate conversion to numpy array (not necessary)

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -113,8 +113,7 @@ class Preprocessor:
         print('Begin dimentionality reduction on tf-idf embeddings...')
 
         # Train an incremental PCA algorithm on the sparse data
-        sparce_embeddings = self.tsvd.fit_transform(self.tfidf_embeddings)
-        self.reduce_tfidf_embeddings = sparce_embeddings.toarray()
+        self.reduce_tfidf_embeddings =  self.tsvd.fit_transform(self.tfidf_embeddings)
 
         print('Finished dimentionality reduction on tf-idf embeddings.')
         print('Begin saving...')
