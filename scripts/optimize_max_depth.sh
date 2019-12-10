5a67f88c1a4ec94b0025aef4d97b90c427869b77
Matheus Centa <matheuscenta@gmail.com>
2019-12-10 12:42:02 +0100

    Add script to optimize max_depth hyperparam

---
+++ scripts/optimize_max_depth.sh
@@ -0,0 +1,6 @@
+python3 train.py --train_name train --val_name val --model_name e1000d3 --n_etimators 1000 --max_depth 3 --gpu
+python3 train.py --train_name train --val_name val --model_name e1000d5 --n_etimators 1000 --max_depth 5 --gpu
+python3 train.py --train_name train --val_name val --model_name e1000d8 --n_etimators 1000 --max_depth 8 --gpu
+python3 train.py --train_name train --val_name val --model_name e1000d10 --n_etimators 1000 --max_depth 10 --gpu
+python3 train.py --train_name train --val_name val --model_name e1000d16 --n_etimators 1000 --max_depth 16 --gpu
+python3 train.py --train_name train --val_name val --model_name e1000d32 --n_etimators 1000 --max_depth 32 --gpu
