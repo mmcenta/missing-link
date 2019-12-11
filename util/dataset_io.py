35e3c6cd8aef71c66554f79c7c02da8be597b312
Matheus Centa <matheuscenta@gmail.com>
2019-12-11 19:44:31 +0100

    Save datasets not as objects

--- util/dataset_io.py
+++ util/dataset_io.py
@@ -1,21 +1,37 @@
 import os
-import pickle
 import numpy as np
 
 
 def save_dataset(name, X, y=None):
     os.makedirs('/home/matheuscenta/missing-link/data/datasets', exist_ok=True)
-    with open("/home/matheuscenta/missing-link/data/datasets/X_" + name + ".pickle", "wb") as f:
-        pickle.dump(X, f)
+
+    with open("/home/matheuscenta/missing-link/data/datasets/X_" + name + ".data", "w") as f:
+        f.write(str(X.shape[0]) + " " + X.shape[1] + "\n")
+        for x in X:
+            f.write(" ".join(list(map(str, x))) + "\n")
+
     if y is not None:
-        with open("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".pickle", "wb") as f:
-            pickle.dump(y, f)
+        with open("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".data", "wb") as f:
+           f.write(str(y.shape[0]) + '\n')
+           for labels in y:
+               f.write(" ".join(list(map(str, labels))) + "\n")
+
 
 def load_dataset(name):
-    with open("/home/matheuscenta/missing-link/data/datasets/X_" + name + ".pickle", "rb") as f:
-        X = pickle.load(f)
+    with open("/home/matheuscenta/missing-link/data/datasets/X_" + name + ".data", "rb") as f:
+        f.readline()
+        X = []
+        for line in f:
+            X.append(list(map(float, line.split())))
+        X = np.array(X)
+
     y = None
-    if os.path.isfile("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".pickle"):
-        with open("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".pickle", "rb") as f:
-            y = pickle.load(f)
+    if os.path.isfile("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".data"):
+        with open("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".data", "rb") as f:
+            f.readline()
+            y = []
+            for line in f:
+                y.append(list(map(int, line.split())))
+            y = np.array(y)
+
     return X, y
