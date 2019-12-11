1b2bc3758c6476aee8c55f3e7c75d7115ad3a7bf
Matheus Centa <matheuscenta@gmail.com>
2019-12-11 20:02:51 +0100

    Bugfix in dataset_io.py

--- util/dataset_io.py
+++ util/dataset_io.py
@@ -8,13 +8,13 @@ def save_dataset(name, X, y=None):
 
     with open("/home/matheuscenta/missing-link/data/datasets/X_" + name + ".data", "w") as f:
         f.write(str(X.shape[0]) + " " + str(X.shape[1]) + "\n")
-        for i in tqdm(range(len(X.shape[0]))):
+        for i in tqdm(range(X.shape[0])):
             f.write(" ".join(list(map(str, X[i]))) + "\n")
 
     if y is not None:
         with open("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".data", "wb") as f:
            f.write(str(y.shape[0]) + '\n')
-           for i in tqdm(range(len(y.shape[0]))):
+           for i in tqdm(range(y.shape[0])):
                f.write(" ".join(list(map(str, y[i]))) + "\n")
 
 
