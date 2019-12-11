433af0e440e6ffefa089f6da17b2398349ffe64d
Matheus Centa <matheuscenta@gmail.com>
2019-12-11 20:39:35 +0100

    Use .npy extension

--- util/dataset_io.py
+++ util/dataset_io.py
@@ -1,23 +1,22 @@
 import os
 import numpy as np
-from tqdm import trange
 
 def save_dataset(name, X, y=None):
     os.makedirs('/home/matheuscenta/missing-link/data/datasets', exist_ok=True)
-    with open("/home/matheuscenta/missing-link/data/datasets/X_" + name + ".data", "wb") as f:
+    with open("/home/matheuscenta/missing-link/data/datasets/X_" + name + ".npy", "wb") as f:
         np.save(f, X)
     if y is not None:
-        with open("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".data", "wb") as f:
+        with open("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".npy", "wb") as f:
             np.save(f, y)
 
 
 def load_dataset(name):
-    with open("/home/matheuscenta/missing-link/data/datasets/X_" + name + ".data", "rb") as f:
+    with open("/home/matheuscenta/missing-link/data/datasets/X_" + name + ".npy", "rb") as f:
         X = np.load(f)
 
     y = None
-    if os.path.isfile("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".data"):
-        with open("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".data", "rb") as f:
+    if os.path.isfile("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".npy"):
+        with open("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".npy", "rb") as f:
             y = np.load(f)
 
     return X, y
