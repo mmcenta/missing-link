f465f14631e5945780f1d8ab7b48ded8e664b323
Matheus Centa <matheuscenta@gmail.com>
2019-12-11 20:35:58 +0100

    Use numpy's binary serializer to save datasets

--- util/dataset_io.py
+++ util/dataset_io.py
@@ -1,38 +1,23 @@
 import os
-import csv
 import numpy as np
 from tqdm import trange
 
 def save_dataset(name, X, y=None):
     os.makedirs('/home/matheuscenta/missing-link/data/datasets', exist_ok=True)
-
-    with open("/home/matheuscenta/missing-link/data/datasets/X_" + name + ".data", "w") as f:
-        out = csv.writer(f)
-        for i in trange(X.shape[0]):
-            out.writerow(X[i])
-
+    with open("/home/matheuscenta/missing-link/data/datasets/X_" + name + ".data", "wb") as f:
+        np.save(f, X)
     if y is not None:
-        with open("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".data", "w") as f:
-            out = csv.writer(f)
-            for i in trange(y.shape[0]):
-               out.writerow(y[i])
+        with open("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".data", "wb") as f:
+            np.save(f, y)
 
 
 def load_dataset(name):
-    with open("/home/matheuscenta/missing-link/data/datasets/X_" + name + ".data", "r") as f:
-        f.readline()
-        X = []
-        for line in f:
-            X.append(list(map(float, line.split())))
-        X = np.array(X)
+    with open("/home/matheuscenta/missing-link/data/datasets/X_" + name + ".data", "rb") as f:
+        X = np.load(f)
 
     y = None
     if os.path.isfile("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".data"):
-        with open("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".data", "r") as f:
-            f.readline()
-            y = []
-            for line in f:
-                y.append(list(map(int, line.split())))
-            y = np.array(y)
+        with open("/home/matheuscenta/missing-link/data/datasets/y_" + name + ".data", "rb") as f:
+            y = np.load(f)
 
     return X, y
