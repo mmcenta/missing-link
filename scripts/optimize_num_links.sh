046009edad4999e49bbba16bb7631c9e5c525606
Matheus Centa <matheuscenta@gmail.com>
2019-12-12 17:11:18 +0100

    Create script to run potential link tests

---
+++ scripts/optimize_num_links.sh
@@ -0,0 +1,35 @@
+sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 1
+deepwalk --format adjlist --input ./data/tmp_adjlist.txt --number_walks 10 --representatio_size 512 --walk_length 40 --window_size 2 --workers 16 --output ./data/node_information/tmp_deepwalk.embeddings
+sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
+sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
+sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec1.txt
+
+sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 2
+deepwalk --format adjlist --input ./data/tmp_adjlist.txt --number_walks 10 --representatio_size 512 --walk_length 40 --window_size 2 --workers 16 --output ./data/node_information/tmp_deepwalk.embeddings
+sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
+sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
+sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec2.txt
+
+sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 3
+deepwalk --format adjlist --input ./data/tmp_adjlist.txt --number_walks 10 --representatio_size 512 --walk_length 40 --window_size 2 --workers 16 --output ./data/node_information/tmp_deepwalk.embeddings
+sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
+sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
+sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec3.txt
+
+sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 4
+deepwalk --format adjlist --input ./data/tmp_adjlist.txt --number_walks 10 --representatio_size 512 --walk_length 40 --window_size 2 --workers 16 --output ./data/node_information/tmp_deepwalk.embeddings
+sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
+sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
+sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec4.txt
+
+sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 5
+deepwalk --format adjlist --input ./data/tmp_adjlist.txt --number_walks 10 --representatio_size 512 --walk_length 40 --window_size 2 --workers 16 --output ./data/node_information/tmp_deepwalk.embeddings
+sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
+sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
+sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec5.txt
+
+sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 6
+deepwalk --format adjlist --input ./data/tmp_adjlist.txt --number_walks 10 --representatio_size 512 --walk_length 40 --window_size 2 --workers 16 --output ./data/node_information/tmp_deepwalk.embeddings
+sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
+sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
+sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec6.txt
--- train_logreg.py
+++ train_logreg.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     print("Done.\nTraining...")
 
 
-    model = LogisticRegression(verbose=1, C=args.C, n_jobs=4, solver='saga')
+    model = LogisticRegression(verbose=0, C=args.C, n_jobs=4, solver='saga')
     model.fit(X_train, y_train)
 
     print("Done.\nSaving...")
