1b0ff6410ade7ed5349b290a043cd165410e4d8c
Matheus Centa <matheuscenta@gmail.com>
2019-12-12 19:00:24 +0100

    Update optimization script

--- scripts/optimize_num_links.sh
+++ scripts/optimize_num_links.sh
@@ -1,35 +1,23 @@
-sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 1
+sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 7
 deepwalk --format adjlist --input ./data/tmp_adjlist.txt --number-walks 10 --representation-size 512 --walk-length 40 --window-size 2 --workers 16 --output ./data/node_information/tmp_deepwalk.embeddings
 sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
-sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec1.txt
+sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec7.txt
 
-sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 2
+sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 8
 deepwalk --format adjlist --input ./data/tmp_adjlist.txt --number-walks 10 --representation-size 512 --walk-length 40 --window-size 2 --workers 16 --output ./data/node_information/tmp_deepwalk.embeddings
 sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
-sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec2.txt
+sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec8.txt
 
-sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 3
+sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 9
 deepwalk --format adjlist --input ./data/tmp_adjlist.txt --number-walks 10 --representation-size 512 --walk-length 40 --window-size 2 --workers 16 --output ./data/node_information/tmp_deepwalk.embeddings
 sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
-sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec3.txt
+sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec9.txt
 
-sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 4
+sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 10
 deepwalk --format adjlist --input ./data/tmp_adjlist.txt --number-walks 10 --representation-size 512 --walk-length 40 --window-size 2 --workers 16 --output ./data/node_information/tmp_deepwalk.embeddings
 sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
-sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec4.txt
-
-sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 5
-deepwalk --format adjlist --input ./data/tmp_adjlist.txt --number-walks 10 --representation-size 512 --walk-length 40 --window-size 2 --workers 16 --output ./data/node_information/tmp_deepwalk.embeddings
-sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
-sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
-sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec5.txt
-
-sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --embeddings_file ./data/node_information/doc2vec.embeddings --num_potential_links 6
-deepwalk --format adjlist --input ./data/tmp_adjlist.txt --number-walks 10 --representation-size 512 --walk-length 40 --window-size 2 --workers 16 --output ./data/node_information/tmp_deepwalk.embeddings
-sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
-sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
-sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec6.txt
+sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > doc2vec10.txt
