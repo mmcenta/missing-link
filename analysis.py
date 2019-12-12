d023f3c7a135c0c9685a03a5a588df18ff7f50e7
Matheus Centa <matheuscenta@gmail.com>
2019-12-12 16:57:23 +0100

    Remove debug code from codebase

--- analysis.py
+++
@@ -1,9 +0,0 @@
-with open("./data/node_information/doc2vec.embeddings", "r") as f:
-    seen = set()
-    num_nodes, dim = tuple(map(int, f.readline().split()))
-    print(num_nodes, dim)
-    for line in f:
-        l = len(line.split())
-        if l not in seen:
-            print(l)
-            seen.add(l)
--- util/embeddings_io.py
+++ util/embeddings_io.py
@@ -30,8 +30,6 @@ def load_embeddings(filepath, key_transform=None, vector_transform=float, header
             num_nodes, dim = tuple(map(int, f.readline().split()))
         for line in f:
             line = line.split()
-            if len(line) < dim:
-                print(len(line), dim)
             key = line[0] if key_transform is None else key_transform(line[0])
             vector = np.array(list(map(vector_transform, line[1:])))
             embeddings[key] = vector
