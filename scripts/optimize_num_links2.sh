e0d29124f89ab39de67a0a144a7264db30a2bbbf
Matheus Centa <matheuscenta@gmail.com>
2019-12-13 00:04:32 +0100

    Bugfix

--- scripts/optimize_num_links2.sh
+++ scripts/optimize_num_links2.sh
@@ -1,5 +1,5 @@
 
-sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt -tfidf_file ./data/node_information/tfidf_embeddings.pickle --k 11
+sudo python3 generate_adjlist.py --input_file ./data/train.txt --output_file ./data/tmp_adjlist.txt --tfidf_file ./data/node_information/tfidf_embeddings.pickle --k 11
 deepwalk --format adjlist --input ./data/tmp_adjlist.txt --number-walks 10 --representation-size 512 --walk-length 40 --window-size 2 --workers 16 --output ./data/node_information/tmp_deepwalk.embeddings
 sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
