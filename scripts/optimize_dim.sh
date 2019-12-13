204fa08b0b862b521f9db249b448ed6fbe1a75b8
Matheus Centa <matheuscenta@gmail.com>
2019-12-13 13:58:04 +0100

    Fix typo in optimize_dim.sh

--- scripts/optimize_dim.sh
+++ scripts/optimize_dim.sh
@@ -1,4 +1,4 @@
-sudo python3 preprocess_test.py --representation_size 64 --use_tfidf --use_doc2vec
+sudo python3 preprocess_text.py --representation_size 64 --use_tfidf --use_doc2vec
 deepwalk --format adjlist --input ./data/train_adjlist.txt --number-walks 10 --representation-size 64 --walk-length 40 --window-size 2 --workers 16
 sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
@@ -12,7 +12,7 @@ sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tm
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > tfidf64.txt
 
-sudo python3 preprocess_test.py --representation_size 128 --use_tfidf --use_doc2vec
+sudo python3 preprocess_text.py --representation_size 128 --use_tfidf --use_doc2vec
 deepwalk --format adjlist --input ./data/train_adjlist.txt --number-walks 10 --representation-size 128 --walk-length 40 --window-size 2 --workers 16
 sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
@@ -26,7 +26,7 @@ sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tm
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > tfidf128.txt
 
-sudo python3 preprocess_test.py --representation_size 256 --use_tfidf --use_doc2vec
+sudo python3 preprocess_text.py --representation_size 256 --use_tfidf --use_doc2vec
 deepwalk --format adjlist --input ./data/train_adjlist.txt --number-walks 10 --representation-size 256 --walk-length 40 --window-size 2 --workers 16
 sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
@@ -40,7 +40,7 @@ sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tm
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > tfidf256.txt
 
-sudo python3 preprocess_test.py --representation_size 512 --use_tfidf --use_doc2vec
+sudo python3 preprocess_text.py --representation_size 512 --use_tfidf --use_doc2vec
 deepwalk --format adjlist --input ./data/train_adjlist.txt --number-walks 10 --representation-size 512 --walk-length 40 --window-size 2 --workers 16
 sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
@@ -54,7 +54,7 @@ sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tm
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 train_logreg.py --train_name tmp --val_name tmp_val > tfidf512.txt
 
-sudo python3 preprocess_test.py --representation_size 1024 --use_tfidf --use_doc2vec
+sudo python3 preprocess_text.py --representation_size 1024 --use_tfidf --use_doc2vec
 deepwalk --format adjlist --input ./data/train_adjlist.txt --number-walks 10 --representation-size 1024 --walk-length 40 --window-size 2 --workers 16
 sudo python3 transform_dataset.py --input_file ./data/train.txt --output_name tmp  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
 sudo python3 transform_dataset.py --input_file ./data/val.txt --output_name tmp_val  --graph_embeddings_file ./data/node_information/tmp_deepwalk.embeddings --concatenate
