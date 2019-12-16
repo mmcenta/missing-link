6cbe40f99025783f48331364a8218050dadf5097
Matheus Centa <matheuscenta@gmail.com>
2019-12-16 20:16:48 +0100

    Change training and val sets

--- scripts/optimize_max_depth.sh
+++ scripts/optimize_max_depth.sh
@@ -1,18 +1,17 @@
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 3 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 1 --gpu
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 4 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 1 --gpu
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 8 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 1 --gpu
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 12 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 1 --gpu
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 16 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 1 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 3 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 1 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 4 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 1 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 8 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 1 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 12 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 1 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 16 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 1 --gpu
 
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 3 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 3 --gpu
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 4 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 3 --gpu
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 8 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 3 --gpu
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 12 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 3 --gpu
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 16 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 3 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 3 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 3 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 4 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 3 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 8 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 3 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 12 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 3 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 16 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 3 --gpu
 
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 3 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --gpu
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 4 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --gpu
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 8 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --gpu
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 12 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --gpu
-sudo python3 train_xgboost.py --train_name concat_train --val_name concat_val --n_estimators 512 --max_depth 16 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --gpu
-
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 3 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 4 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 8 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 12 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512 --max_depth 16 --learning_rate 0.1 --subsample 0.8 --colsample_bytree 0.8 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --gpu
