51cf396d58c2170948fab2237d2e81a014a85794
Matheus Centa <matheuscenta@gmail.com>
2019-12-29 21:41:54 +0100

    Add final experiment script

---
+++ scripts/regularization.sh
@@ -0,0 +1,8 @@
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512  --max_depth 12 --learning_rate 0.1 --subsample 0.3 --colsample_bytree 0.5 --gamma 10 --scale_pos_weight 1 --min_child_weight 5 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512  --max_depth 12 --learning_rate 0.1 --subsample 0.3 --colsample_bytree 0.5 --gamma 100 --scale_pos_weight 1 --min_child_weight 5 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512  --max_depth 12 --learning_rate 0.1 --subsample 0.3 --colsample_bytree 0.5 --gamma 1000 --scale_pos_weight 1 --min_child_weight 5 --gpu
+
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512  --max_depth 12 --learning_rate 0.1 --subsample 0.3 --colsample_bytree 0.5 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --reg_alpha 0.3 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512  --max_depth 12 --learning_rate 0.1 --subsample 0.3 --colsample_bytree 0.5 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --reg_alpha 0.5 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512  --max_depth 12 --learning_rate 0.1 --subsample 0.3 --colsample_bytree 0.5 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --reg_alpha 0.7 --gpu
+sudo python3 train_xgboost.py --train_name train --val_name val --n_estimators 512  --max_depth 12 --learning_rate 0.1 --subsample 0.3 --colsample_bytree 0.5 --gamma 0 --scale_pos_weight 1 --min_child_weight 5 --reg_alpha 1.0 --gpu
